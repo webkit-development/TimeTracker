af2d045869c2434f363b8f0c3864aa4acb425c5d
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-27 12:43:56 -0400

    Initial Commit

---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Kevin Stradtman on 3/27/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
